d322a43e755d8cd74b9ed7b32eea0a0be9da177b
Jaex <Jaex@server.fake>
2013-11-07 18:51:28 +0200

    Fixed sidebar responsive

--- js/loader.js
+++ js/loader.js
@@ -1,24 +1,26 @@
 $(document).ready(function () {
     var isLoading = false;
     var hash = window.location.hash.substr(1) + ".html";
-    $("#sidebar li a").each(function () {
+    $("#sidebar a").each(function () {
         var href = $(this).attr("href");
         if (hash == href) {
             LoadContent(href);
+            ToggleActive($(this));
             isLoading = true;
             return false;
         }
     });
 
     if (!isLoading) {
-        var href = $("#sidebar li a").first().attr("href");
+        var href = $("#sidebar a").first().attr("href");
         LoadContent(href);
     }
 
-    $("#sidebar li a").click(function () {
+    $("#sidebar a").click(function () {
         window.scrollTo(0, 0);
         var href = $(this).attr("href");
         LoadContent(href);
+        ToggleActive($(this));
         return false;
     });
 });
@@ -36,5 +38,12 @@ function LoadContent(href) {
     function showNewContent() {
         $("#loading").hide();
         $("#content").fadeIn("normal");
+    }
+}
+
+function ToggleActive(a) {
+    $("#sidebar a.active").removeClass("active");
+    if (!a.hasClass('active')) {
+        a.addClass('active');
     }
 }
