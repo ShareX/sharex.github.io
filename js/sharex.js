d89712c4fa4c3bb1f2360dd62b2f8e642419ebcd
Jaex <Jaex@server.fake>
2013-11-17 18:30:04 +0200

    Using ajax to download github release download count

--- js/sharex.js
+++ js/sharex.js
@@ -1,55 +1,12 @@
-$("#sidebar").affix({
-    offset: {
-        top: $('#top-navbar').height() + $('.jumbotron').height()
-    }
-});
-
-$(document).ready(function () {
-    var isLoading = false;
-    var hash = window.location.hash.substr(1) + ".html";
-    $("#sidebar a").each(function () {
-        var href = $(this).attr("href");
-        if (hash == href) {
-            LoadContent(href);
-            ToggleActive($(this));
-            isLoading = true;
-            return false;
-        }
-    });
-
-    if (!isLoading) {
-        var href = $("#sidebar a").first().attr("href");
-        LoadContent(href);
-    }
-
-    $("#sidebar a").click(function () {
-        window.scrollTo(0, 0);
-        var href = $(this).attr("href");
-        LoadContent(href);
-        ToggleActive($(this));
-        return false;
+﻿$(document).ready(function () {
+    $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases").done(function (json) {
+        var latestRelease = json[0];
+        var name = latestRelease.name;
+        var asset = latestRelease.assets[0];
+        var date = new Date(asset.updated_at);
+        var downloadCount = asset.download_count;
+        var text = name + " was last updated on " + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " and has been downloaded " + downloadCount + " times.";
+        $(".release-info").text(text);
+        $(".release-info").slideDown("slow");
     });
 });
-
-function LoadContent(href) {
-    $("#content").fadeOut("fast", loadContent);
-    $("#loading").fadeIn("fast");
-
-    window.location.hash = href.substr(0, href.length - 5);
-
-    function loadContent() {
-        $("#content").load(href, "", showNewContent())
-    }
-
-    function showNewContent() {
-        $("#loading").hide();
-        $("#content").fadeIn("normal");
-    }
-}
-
-function ToggleActive(a) {
-    $("#sidebar a.active").removeClass("active");
-    if (!a.hasClass('active')) {
-        a.addClass('active');
-    }
-}
