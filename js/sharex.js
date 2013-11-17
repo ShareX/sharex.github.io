9e5652a608fd35b7c42100c1b5058b9c51d63a60
Jaex <Jaex@server.fake>
2013-11-17 19:05:32 +0200

    Comment sharex.js until release new version

--- js/sharex.js
+++ js/sharex.js
@@ -5,7 +5,7 @@
         var asset = latestRelease.assets[0];
         var date = new Date(asset.updated_at);
         var downloadCount = asset.download_count;
-        var text = name + " was last updated on " + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " and has been downloaded " + downloadCount + " times.";
+        var text = name + " released on " + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " and downloaded " + downloadCount + " times.";
         $(".release-info").text(text);
         $(".release-info").fadeIn("slow");
     });
