ec84c91689854534038f09fcac85eece7b584da5
Jaex <Jaex@server.fake>
2013-11-20 22:41:21 +0200

    Getting latest direct download url with ajax

--- js/sharex.js
+++ js/sharex.js
@@ -4,14 +4,15 @@
 
 function GetLatestReleaseInfo() {
     $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases").done(function (json) {
-        var latestRelease = json[0];
-        var name = latestRelease.name;
-        var asset = latestRelease.assets[0];
-        var timeago = $.timeago(asset.updated_at);
-        var downloadCount = asset.download_count;
-        var text = name + " was released " + timeago + " and downloaded " + downloadCount + " times.";
-        $(".sharex-screenshot").animate({ "top": "+=15px" });
-        $(".release-info").text(text);
+        var release = json[0];
+        var asset = release.assets[0];
+
+        var downloadURL = "https://github.com/ShareX/ShareX/releases/download/" + release.tag_name + "/" + asset.name;
+        $(".sharex-download").attr("href", downloadURL);
+
+        var releaseInfo = release.name + " was released " + $.timeago(asset.updated_at) + " and downloaded " + asset.download_count + " times.";
+        $("#sharex-screenshot").animate({ "top": "+=15px" });
+        $(".release-info").text(releaseInfo);
         $(".release-info").fadeIn("slow");
     });
 }
