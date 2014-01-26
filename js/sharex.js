2eb2018467dd2b5684ea3c4e59416d3b2502e562
Jaex <Jaex@server.fake>
2014-01-26 12:09:31 +0200

    Download count will include portable download count too

--- js/sharex.js
+++ js/sharex.js
@@ -6,11 +6,13 @@ function GetLatestReleaseInfo() {
     $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases").done(function (json) {
         var release = json[0];
         var asset = release.assets[0];
-
         var downloadURL = "https://github.com/ShareX/ShareX/releases/download/" + release.tag_name + "/" + asset.name;
+        var downloadCount = 0;
+        for (var i = 0; i < release.assets.length; i++) {
+            downloadCount += release.assets[i].download_count;
+        }
+        var releaseInfo = release.name + " was updated " + $.timeago(asset.updated_at) + " and downloaded " + downloadCount + " times.";
         $(".sharex-download").attr("href", downloadURL);
-
-        var releaseInfo = release.name + " was updated " + $.timeago(asset.updated_at) + " and downloaded " + asset.download_count + " times.";
         $("#sharex-screenshot").animate({ "top": "+=20px" });
         $(".release-info").text(releaseInfo);
         $(".release-info").fadeIn("slow");
