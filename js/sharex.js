203640012e5047011e7247c247a977e7ae3f03af
Jaex <Jaex@users.noreply.github.com>
2016-06-15 19:56:45 +0300

    Scroll animation improvements

--- js/sharex.js
+++ js/sharex.js
@@ -3,6 +3,53 @@
 
 "use strict";
 
+var $window = $(window);
+var $animation_elements = $(".animation-element");
+
+function SlideCheck() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = window_top_position + window_height;
+
+    $.each($animation_elements, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = element_top_position + element_height;
+
+        if (element_bottom_position >= window_top_position &&
+            element_top_position <= window_bottom_position) {
+            $element.addClass("slide");
+        } else {
+            $element.removeClass("slide");
+        }
+    });
+}
+
+function GetLatestReleaseInfo() {
+    $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases/latest").done(function (release) {
+        var asset = release.assets[0];
+        var downloadCount = 0;
+        for (var i = 0; i < release.assets.length; i++) {
+            downloadCount += release.assets[i].download_count;
+        }
+        var oneHour = 60 * 60 * 1000;
+        var oneDay = 24 * oneHour;
+        var dateDiff = new Date() - new Date(asset.updated_at);
+        var timeAgo;
+        if (dateDiff < oneDay) {
+            timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
+        } else {
+            timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
+        }
+        var releaseInfo = release.name + " was updated " + timeAgo + " and has been downloaded " + downloadCount.toLocaleString() + " times.";
+        $(".sharex-download").attr("href", asset.browser_download_url);
+        $(".sharex-download").prop("title", releaseInfo);
+
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+}
+
 $(document).ready(function () {
     SlideCheck();
 
@@ -51,38 +98,4 @@ $(document).ready(function () {
     GetLatestReleaseInfo();
 });
 
-$(window).scroll(SlideCheck);
-
-function SlideCheck() {
-    $(".slide-anim").each(function () {
-        var pos = $(this).offset().top;
-        var winTop = $(window).scrollTop();
-        if (pos < winTop + $(window).height()) {
-            $(this).addClass("slide");
-        }
-    });
-}
-
-function GetLatestReleaseInfo() {
-    $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases/latest").done(function (release) {
-        var asset = release.assets[0];
-        var downloadCount = 0;
-        for (var i = 0; i < release.assets.length; i++) {
-            downloadCount += release.assets[i].download_count;
-        }
-        var oneHour = 60 * 60 * 1000;
-        var oneDay = 24 * oneHour;
-        var dateDiff = new Date() - new Date(asset.updated_at);
-        var timeAgo;
-        if (dateDiff < oneDay) {
-            timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
-        } else {
-            timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
-        }
-        var releaseInfo = release.name + " was updated " + timeAgo + " and has been downloaded " + downloadCount.toLocaleString() + " times.";
-        $(".sharex-download").attr("href", asset.browser_download_url);
-        $(".sharex-download").prop("title", releaseInfo);
-
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-}
+$window.on("scroll resize", SlideCheck);
