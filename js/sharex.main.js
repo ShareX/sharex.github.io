5d43ab8aba2aa22c358e8f89105ab6eb20c436da
Jaex <Jaex@users.noreply.github.com>
2022-09-14 14:46:59 +0300

    Added GetAppVeyorArtifact function

--- js/sharex.main.js
+++ js/sharex.main.js
@@ -1,5 +1,6 @@
 $(function() {
     GetLatestReleaseInfo();
+    GetAppVeyorArtifact("ShareX", "sharex", "develop", "Setup", $(".sharex-dev-build"));
 });
 
 function GetLatestReleaseInfo() {
@@ -10,8 +11,8 @@ function GetLatestReleaseInfo() {
 }
 
 function UpdateDownloadButton(release, element, assetExtension) {
-    var asset = release.assets.find(asset => asset.name.endsWith(assetExtension));
-    var releaseInfo = "Version: " + release.tag_name.substring(1) +
+    let asset = release.assets.find(asset => asset.name.endsWith(assetExtension));
+    let releaseInfo = "Version: " + release.tag_name.substring(1) +
         "\nFile size: " + (asset.size / 1024 / 1024).toFixed(2) + " MB" +
         "\nRelease date: " + new Date(asset.updated_at).toLocaleDateString("en-CA") +
         "\nDownload count: " + asset.download_count.toLocaleString();
@@ -20,5 +21,25 @@ function UpdateDownloadButton(release, element, assetExtension) {
     element.attr("title", releaseInfo);
     element.tooltip({
         trigger: "hover"
+    });
+}
+
+function GetAppVeyorArtifact(accountName, projectSlug, branch, deploymentName, element) {
+    let apiUrl = "https://ci.appveyor.com/api";
+    $.getJSON(`${apiUrl}/projects/${accountName}/${projectSlug}/branch/${branch}`).done(function(project) {
+        let jobId = project.build.jobs[0].jobId;
+        $.getJSON(`${apiUrl}/buildjobs/${jobId}/artifacts`).done(function(artifacts) {
+            let artifact = artifacts.find(artifact => artifact.name === deploymentName);
+            let artifactFileName = artifact.fileName;
+            let downloadURL = `${apiUrl}/buildjobs/${jobId}/artifacts/${artifactFileName}`;
+            let artifactInfo = "File name: " + artifactFileName +
+                "\nFile size: " + (artifact.size / 1024 / 1024).toFixed(2) + " MB";
+
+            element.attr("href", downloadURL);
+            element.attr("title", artifactInfo);
+            element.tooltip({
+                trigger: "hover"
+            });
+        });
     });
 }
