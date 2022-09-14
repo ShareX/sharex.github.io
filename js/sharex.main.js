5f15a78f36a25982dc30a119b4c284309229191a
Jaex <Jaex@users.noreply.github.com>
2022-09-14 12:32:16 +0300

    Added download panel

--- js/sharex.main.js
+++ js/sharex.main.js
@@ -4,59 +4,21 @@ $(function() {
 
 function GetLatestReleaseInfo() {
     $.getJSON("https://api.github.com/repos/ShareX/ShareX/releases/latest").done(function(release) {
-        var asset = release.assets.find(asset => asset.name.endsWith(".exe"));
-        var downloadCount = 0;
-        for (var i = 0; i < release.assets.length; i++) {
-            downloadCount += release.assets[i].download_count;
-        }
-        var releaseDate = new Date(asset.updated_at).toLocaleDateString("en-CA");
-        var oneHour = 60 * 60 * 1000;
-        var oneDay = 24 * oneHour;
-        var dateDiff = new Date() - new Date(asset.updated_at);
-        var timeAgo;
-        if (dateDiff < oneDay) {
-            timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
-        } else {
-            timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
-        }
-
-        var releaseInfo = "Version: " + release.tag_name.substring(1) +
-            "\nFile size: " + (asset.size / 1024 / 1024).toFixed(2) + " MB" +
-            "\nRelease date: " + releaseDate +
-            "\nReleased: " + timeAgo +
-            "\nDownload count: " + downloadCount.toLocaleString();
-
-        $(".sharex-download").attr("href", asset.browser_download_url);
-        $(".sharex-download").attr("title", "<a href=\"downloads/\"><div>" + releaseInfo + "</div></a>");
-
-        InitTooltip($(".sharex-download"), 300);
+        UpdateDownloadButton(release, $(".sharex-setup"), ".exe");
+        UpdateDownloadButton(release, $(".sharex-portable"), ".zip");
     });
 }
 
-function InitTooltip(obj, fadeDelay) {
-    obj.tooltip({
-        trigger: "manual",
-        html: true,
-        animation: false
-    }).on("mouseenter", function() {
-        obj.tooltip("show");
-    }).on("mouseleave", function() {
-        setTimeout(function() {
-            if (!obj.is(":hover") && $(".tooltip").length && !$(".tooltip").is(":hover")) {
-                obj.tooltip("hide");
-            }
-        }, fadeDelay);
-    });
+function UpdateDownloadButton(release, element, assetExtension) {
+    var asset = release.assets.find(asset => asset.name.endsWith(assetExtension));
+    var releaseInfo = "Version: " + release.tag_name.substring(1) +
+        "\nFile size: " + (asset.size / 1024 / 1024).toFixed(2) + " MB" +
+        "\nRelease date: " + new Date(asset.updated_at).toLocaleDateString("en-CA") +
+        "\nDownload count: " + asset.download_count.toLocaleString();
 
-    $(document).on("mouseleave", ".tooltip", function() {
-        setTimeout(function() {
-            if (!obj.is(":hover") && $(".tooltip").length && !$(".tooltip").is(":hover")) {
-                obj.tooltip("hide");
-            }
-        }, fadeDelay);
+    element.attr("href", asset.browser_download_url);
+    element.attr("title", releaseInfo);
+    element.tooltip({
+        trigger: "hover"
     });
-
-    if (obj.is(":hover")) {
-        obj.tooltip("show");
-    }
 }
