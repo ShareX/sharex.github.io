8c626a25cce4c558a8d5425c1bb5b8566a111aac
Jaex <Jaex@users.noreply.github.com>
2022-10-10 11:16:52 +0300

    Fetch release info on button click

--- js/sharex.main.js
+++ js/sharex.main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    GetLatestReleaseInfo();
+    $(".sharex-download").one("click", function() {
+        GetLatestReleaseInfo();
+    });
 });
 
 function GetLatestReleaseInfo() {
