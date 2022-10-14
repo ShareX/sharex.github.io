217378f7ec86cf49d481c2ddecb505f1bb9c79eb
Jaex <Jaex@users.noreply.github.com>
2022-10-14 21:40:07 +0300

    Fetch release info on button mouse enter

--- js/sharex.main.js
+++ js/sharex.main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $(".sharex-download").one("click", function() {
+    $(".sharex-download").on("mouseenter click", function() {
+        $(this).off("mouseenter click");
+
         GetLatestReleaseInfo();
     });
 });
